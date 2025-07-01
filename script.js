430a5c7d0bc84fc8e281754f9e1a3882a22f90af
Edgar Aguilar <eaguilar@arrivelogistics.com>
2025-07-01 15:52:45 -0600

    Refactor CSS variable updater using dynamic handler

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const spacingInput = document.getElementById('spacing');
-const spacingValue = document.getElementById('spacing-output');
-const blurInput = document.getElementById('blur');
-const blurValue = document.getElementById('blur-output');
-
-const inputs = document.querySelectorAll('.container input');
-
-spacingValue.textContent = spacingInput.value;
-blurValue.textContent = blurInput.value;
+const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(
     `--${this.name}`,
-    this.value + suffix
+    `${this.value}${suffix}`
   );
 }
+
+inputs.forEach((input) => input.addEventListener('input', handleUpdate));
